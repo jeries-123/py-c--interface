8d47062786b3d4588e1d546c16284527fea15ef3
jeries-123 <jeriest59@gmail.com>
2024-04-27 17:29:27 +0300

    Done

--- py.cpp
+++ py.cpp
@@ -6,25 +6,27 @@ int main() {
     // Initialize Python interpreter
     Py_Initialize();
 
-   
-    std::ifstream scriptFile("trash_detection.py");
+    // Add the directory containing take_photo.py to the Python system path
+    PyRun_SimpleString("import sys\n"
+                       "sys.path.append('/home/jeries/Desktop/Project/py-c--interface/')\n");
+
+    // Read the content of the Python script
+    std::ifstream scriptFile("take_photo.py");
     if (!scriptFile.is_open()) {
-        std::cerr << "Failed to open Python trash detection script file\n";
+        std::cerr << "Failed to open Python script file\n";
         return 1;
     }
 
-    // Read the content of the Python script
     std::string scriptContent((std::istreambuf_iterator<char>(scriptFile)),
                               std::istreambuf_iterator<char>());
 
     // Close the file
     scriptFile.close();
 
-    // Run Python trash detection code
-     PyRun_SimpleString("import take_photo\n"
-                       "take_photo.take_photo('photo.jpg')\n");
+    // Run Python script
+    PyRun_SimpleString(scriptContent.c_str());
 
-   
+    // Finalize Python interpreter
     Py_Finalize();
 
     return 0;
--- take_photo.py
+++ take_photo.py
@@ -1,14 +1,12 @@
 import cv2
 
 def take_photo(file_path):
-   
     cap = cv2.VideoCapture(0)
 
     if not cap.isOpened():
         print("Error: Unable to open camera")
         return
 
-  
     ret, frame = cap.read()
 
     if not ret:
@@ -16,14 +14,12 @@ def take_photo(file_path):
         cap.release()
         return
 
-   
     cv2.imwrite(file_path, frame)
 
     print(f"Photo captured and saved to {file_path}")
 
-    # Release the camera
     cap.release()
 
 if __name__ == "__main__":
-    file_path = "photo.jpg"  
+    file_path = "/home/jeries/Desktop/Project/py-c--interface/photo.jpg"
     take_photo(file_path)
